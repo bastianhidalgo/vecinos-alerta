fe2db6e8930ff1d0dfaff8830a99737230cfc159
Alexander Jerez <118144192+XanderJerez@users.noreply.github.com>
2022-11-28 08:00:39 -0300

    Delete asamblea/Asamblea directory

--- asamblea/Asamblea/routes/asambleaRoutes.js
+++
@@ -1,8 +0,0 @@
-const asambleaController = require('../controllers/asamController');
-const express = require('express');
-const api= express.Router();
-
-
-api.post('/asamblea',asambleaController.createAsamblea);
-api.get('/asamblea', asambleaController.getAsambleas);
-module.exports=api;
